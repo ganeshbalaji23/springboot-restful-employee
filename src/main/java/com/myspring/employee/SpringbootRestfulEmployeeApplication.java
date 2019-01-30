3ac9434f7e90bc4f3e0406333a2ea1b18ffdf6df
Ganesh Dhandapani <ganeshbalaji23@gmail.com>
2019-01-30 07:18:41 +0530

    Initial Commit
    Initial Commit

---
+++ src/main/java/com/myspring/employee/SpringbootRestfulEmployeeApplication.java
@@ -0,0 +1,15 @@
+package com.myspring.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestfulEmployeeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(SpringbootRestfulEmployeeApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/myspring/employee/SpringbootRestfulEmployeeApplicationTests.java
@@ -0,0 +1,17 @@
+package com.myspring.employee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootRestfulEmployeeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
